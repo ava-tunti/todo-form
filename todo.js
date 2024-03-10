76187a9e81141b4e39e0df5f131e2fd3ecef6fd9
Ava Tuntikanokporn <150287550+ava-tunti@users.noreply.github.com>
2024-03-09 16:07:30 -0800

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,7 +1,21 @@
+// function Todo({todo,index,remove}){
+//   function handle(){
+//     console.log('Ping:',index);
+//     remove(index);
+//   }
+//   return <div className="todo" onClick={handle}>{todo.text} (-)</div>
+// }
+
 function Todo({todo,index,remove}){
-  function handle(){
+  function handleRemove(){
     console.log('Ping:',index);
     remove(index);
   }
-  return <div className="todo" onClick={handle}>{todo.text} (-)</div>
+
+  return (
+    <div className="todo">
+      <span>{todo.text}</span>
+      <button onClick={handleRemove}>Remove</button>
+    </div>
+  );
 }
