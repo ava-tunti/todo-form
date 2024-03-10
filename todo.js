b31d7a1b173735bf5a39db6fd8d8dfafc3f48582
Ava Tuntikanokporn <150287550+ava-tunti@users.noreply.github.com>
2024-03-09 16:13:04 -0800

    Add files via upload

--- todo.js
+++ todo.js
@@ -6,16 +6,39 @@
 //   return <div className="todo" onClick={handle}>{todo.text} (-)</div>
 // }
 
-function Todo({todo,index,remove}){
-  function handleRemove(){
-    console.log('Ping:',index);
-    remove(index);
-  }
+// function Todo({todo,index,remove}){
+//   function handleRemove(){
+//     console.log('Ping:',index);
+//     remove(index);
+//   }
 
+//   return (
+//     <div className="todo">
+//       <span>{todo.text}</span>
+//       <button onClick={handleRemove}>Remove</button>
+//     </div>
+//   );
+// }
+
+// function Todo({todo, index, remove}) {
+//   // This function will call the remove function passed from the parent component
+//   function handleRemove() {
+//     remove(index);
+//   }
+
+//   return (
+//     <div className="todo">
+//       {todo.text}
+//       <button onClick={handleRemove} className="remove-btn">Remove</button>
+//     </div>
+//   );
+// }
+
+function Todo({ todo, index, remove }) {
   return (
     <div className="todo">
       <span>{todo.text}</span>
-      <button onClick={handleRemove}>Remove</button>
+      <button onClick={() => remove(index)}>Remove</button>
     </div>
   );
 }
